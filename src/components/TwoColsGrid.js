1250ec3b4950a436ca17c355624fbfa26b568a21
Superzzp <zhangzhipian1@gmail.com>
2022-06-04 22:50:36 -0700

    update UI, set twoColGrid to styled components

--- src/components/TwoColsGrid.js
+++ src/components/TwoColsGrid.js
@@ -1,14 +1,28 @@
-import "./TwoColsGrid.css";
+import styled from "styled-components";
 
-function TwoColsGrid (props) {
+function TwoColsGrid(props) {
     return (
-        <div className="response">
-            <div className="col1">Prompt:</div>
-            <div className="col2">{props.prompt}</div>
-            <div className="col1">Response:</div>
-            <div className="col2">{props.response}</div>
-        </div>
+        <GridContainer>
+            <FirstColGrid>Prompt:</FirstColGrid>
+            <div>{props.prompt}</div>
+            <FirstColGrid>Response:</FirstColGrid>
+            <div>{props.response}</div>
+        </GridContainer>
     );
 }
 
+const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: 25% 75%;
+    margin: 20px 0px 0px 0px;
+    padding: 20px;
+    color: white;
+    background-color: #234099;
+    row-gap: 10px;
+    border-radius: 5px;
+`
+
+const FirstColGrid = styled.div`
+    font-weight: bold;
+`
 export default TwoColsGrid;
